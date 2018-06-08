94a0a6e04258a685302b0712ec1df7574ed2846e
JoseJR12 <amanquichua@gmail.com>
2018-06-08 15:43:22 -0500

    base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MisionVisionComponent } from './mision-vision/mision-vision.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: InicioComponent},
+  { path:'mision-vision', component: MisionVisionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+
+
+import { CarouselComponent } from './carousel/carousel.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { MisionVisionComponent } from './mision-vision/mision-vision.component';
+import { DocumentosgestionComponent } from './documentosgestion/documentosgestion.component';
+import { AseguramientoComponent } from './aseguramiento/aseguramiento.component';
+import { SaludComponent } from './salud/salud.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    
+   
+    CarouselComponent,
+    
+   
+    NoticiasComponent,
+    
+   
+    MisionVisionComponent,
+    
+   
+    DocumentosgestionComponent,
+    
+   
+    AseguramientoComponent,
+    
+   
+    SaludComponent,
+    
+   
+  
   ],
   imports: [
     BrowserModule,
